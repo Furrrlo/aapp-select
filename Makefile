fff6ec6b9c076eb7d52330d1f05e470d39df6b6b
Furrrlo <ferloo@protonmail.com>
2024-10-17 23:06:40 +0200

    Use text fixtures to init stuff

--- Makefile
+++ Makefile
@@ -20,7 +20,11 @@ b64:
 
 benchmark:
 	$(CC) -O2 select.c benchmark.c $(BENCH_CFLAGS) -o benchmark.out
-	./benchmark.out --benchmark_out=benchmark.json --benchmark_out_format=json
+	taskset -c 0 \
+		./benchmark.out \
+		--benchmark_out=benchmark.json \
+		--benchmark_out_format=json
+
 clean:
 	rm -f test.out benchmark.out notebook.ipynb
 
--- benchmark.c
+++ benchmark.c
@@ -4,44 +4,66 @@
 extern int *our_select(int arr[], size_t len, int rank);
 extern int *rand_select(int arr[], size_t len, int rank);
 
-static void BM_select_impl(benchmark::State& state, bool use_rand_select)
+class SelectFixture : public benchmark::Fixture
 {
-  // open the random fd
-  FILE *rnd_fd = fopen("/dev/urandom", "r");
-  if(!rnd_fd)
+private:
+  FILE *m_rnd_fd;
+  int *m_rnd_arr;
+  int m_len;
+public:
+  void SetUp(::benchmark::State& state)
   {
-    state.SkipWithError("Failed to open /dev/urandom");
-    return;
+    // seed the rand() function for use within rand_select()
+    srand(time(NULL));
+
+    // open the random fd
+    m_rnd_fd = fopen("/dev/urandom", "r");
+    if(!m_rnd_fd)
+    {
+      state.SkipWithError("Failed to open /dev/urandom");
+      return;
+    }
+
+    // Allocate an array of len given by the benchmark lib
+    m_len = state.range(0);
+    m_rnd_arr = (int*) malloc(m_len * sizeof(*m_rnd_arr));
+    if(!m_rnd_arr)
+    {
+      state.SkipWithError("Failed to allocate random array");
+      return;
+    }
   }
 
-  // Allocate an array of len given by the benchmark lib
-  int len = state.range(0);
-  int *rnd_arr = (int*) malloc(len * sizeof(*rnd_arr));
-  if(!rnd_arr)
+  void TearDown(::benchmark::State& state)
   {
-    state.SkipWithError("Failed to allocate random array");
-    return;
+    free(m_rnd_arr);
+    fclose(m_rnd_fd);
   }
- 
-  // Run the benchmark
+
+  int len() { return m_len; }
+  int *generate_random_array()
+  {
+    // generate a random array with nums in range [-len:len]
+    int nread = 0;
+    while(nread < m_len)
+      nread = fread(&m_rnd_arr[nread], sizeof(*m_rnd_arr), m_len - nread, m_rnd_fd);
+    for(size_t i = 0; i < m_len; ++i)
+      m_rnd_arr[i] = abs(m_rnd_arr[i]) % (m_len * 2) - m_len;
+    return m_rnd_arr;
+  }
+};
+
+static void BM_select_impl(SelectFixture *fixture, benchmark::State& state, bool use_rand_select)
+{
+  int len = fixture->len();
   for (auto _ : state) {
     state.PauseTiming();
-    
-    // generate a random array 
-    int nread = 0;
-    while(nread < len)
-      nread = fread(&rnd_arr[nread], sizeof(*rnd_arr), len - nread, rnd_fd);
-
-    // pick a random number which is inside the [0:len) range
-    int rnd_pick;
-    while(!fread(&rnd_pick, sizeof(rnd_pick), 1, rnd_fd));
-    rnd_pick = 1 + (abs(rnd_pick) % len);
-
+    int *rnd_arr = fixture->generate_random_array();
     state.ResumeTiming();
 
     int *res = use_rand_select
-      ? rand_select(rnd_arr, len, rnd_pick)
-      : our_select(rnd_arr, len, rnd_pick);
+      ? rand_select(rnd_arr, len, 1 + (len / 2))
+      : our_select(rnd_arr, len, 1 + (len / 2));
     if(res == NULL) {
       state.SkipWithError("Result is null, probably an allocation failure");
       break;
@@ -51,30 +73,19 @@ static void BM_select_impl(benchmark::State& state, bool use_rand_select)
   }
 
   state.SetComplexityN(len);
-  free(rnd_arr);
-  fclose(rnd_fd);
 }
 
-static void BM_select(benchmark::State& state)
-{
-  BM_select_impl(state, false);
-}
-
-static void BM_rand_select(benchmark::State& state)
-{
-  // seed the rand() function for use within rand_select()
-  srand(time(NULL));
-  BM_select_impl(state, true);
-}
-
-BENCHMARK(BM_select)
+BENCHMARK_DEFINE_F(SelectFixture, BM_select)(benchmark::State& state) { BM_select_impl(this, state, false); }
+BENCHMARK_REGISTER_F(SelectFixture, BM_select)
   ->RangeMultiplier(2)
-  ->Range(1 << 10, 1 << 20)
+  ->Range(1 << 2, 1 << 18)
   ->Complexity();
 
-BENCHMARK(BM_rand_select)
+
+BENCHMARK_DEFINE_F(SelectFixture, BM_rand_select)(benchmark::State& state) { BM_select_impl(this, state, true); }
+BENCHMARK_REGISTER_F(SelectFixture, BM_rand_select)
   ->RangeMultiplier(2)
-  ->Range(1 << 10, 1 << 20)
+  ->Range(1 << 2, 1 << 18)
   ->Complexity();
 
 BENCHMARK_MAIN();
